544c9a2e6c9fa632e86002bc5329b6ae6b5e2848
Alex <tj85967@yahoo.com.tw>
2021-10-17 17:33:34 +0800

    test

--- Demo1/小專.js
+++ Demo1/小專.js
@@ -1,18 +1,24 @@
 
-const text = document.querySelectorAll(".thePaths");
+// const text = document.querySelectorAll(".thePaths");
 
-for(let k = 0; k < text.length; k++){
-    console.log(`text number ${k} length is ${text[k].getTotalLength()}`);
-}
+// for(let k = 0; k < text.length; k++){
+//     console.log(`text number ${k} length is ${text[k].getTotalLength()}`);
+// }
 
-const lastWord = document.querySelector("#twenty");
+const lastWord = document.querySelector("#twentyone");
 const animation = document.querySelector("div.animation");
 lastWord.addEventListener("animationend",() => {
     animation.style = "transition:all 2.5s ease; opacity:0; pointer-events:none;";
 })
 
+const lastWordMob = document.querySelector("#twentyoneMob");
+const animationMob = document.querySelector("div.animationMob");
+lastWordMob.addEventListener("animationend",() => {
+    animationMob.style = "transition:all 2.5s ease; opacity:0; pointer-events:none;";
+})
 
-function openSeason(evt, seasonName) {
+
+  function openSeason(evt, seasonName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -25,6 +31,22 @@ function openSeason(evt, seasonName) {
     document.getElementById(seasonName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  
+function openSeason2(evt, seasonName) {
+  var i, tabcontentMob, tablinksMob;
+  tabcontentMob = document.getElementsByClassName("tabcontentMob");
+  for (i = 0; i < tabcontentMob.length; i++) {
+    tabcontentMob[i].style.display = "none";
+  }
+  tablinksMob = document.getElementsByClassName("tablinksMob");
+  for (i = 0; i < tablinksMob.length; i++) {
+    tablinksMob[i].className = tablinksMob[i].className.replace(" active", "");
+  }
+  document.getElementById(seasonName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
 
 // --------------------
 const swiper = new Swiper('.swiper-container', {
